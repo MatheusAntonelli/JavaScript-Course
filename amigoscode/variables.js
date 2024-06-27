5a2927e7e5b4759fe6ccfc40c902b3a5e3f45a1b
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-06-26 21:59:44 -0300

    Variables class

---
+++ amigoscode/variables.js
@@ -0,0 +1,22 @@
+//String
+var firstName = "Matheus";
+var lastName = "Antonelli";
+//Number
+var age = 22;
+var balance = 23.2;
+//Boleean
+var isMale = true
+
+//Dates
+var dob = new Date(2002, 2,25);
+
+var person = { firstName, lastName, age, balance};
+var empty = undefined;
+
+console.log(firstName + " " + lastName);
+console.log(age);
+console.log(isMale);
+console.log(balance);
+console.log(dob);
+console.log(person);
+console.log(empty);
