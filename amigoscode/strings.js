78eb3a61ceb465d7b95a07d0db26b4479950992c
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-06-28 00:43:00 -0300

    strings.js

---
+++ amigoscode/strings.js
@@ -0,0 +1,11 @@
+//Strings
+var brand = "AmigosCode"
+console.log(typeof brand);
+console.log(brand.length);
+console.log(brand.toUpperCase());
+console.log(brand.substring(0,4));
+
+var a = "Amigos";
+var b = "Code"
+console.log(a + " " + b);
+console.log( `${a} ${b.toUpperCase()}`);
