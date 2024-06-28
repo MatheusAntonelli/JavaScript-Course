f75efe6884eac7635c9c5ff007337319b52e6d32
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-06-28 00:59:59 -0300

    boolean.js

---
+++ amigoscode/boolean.js
@@ -0,0 +1,18 @@
+// Boleean
+var isAdult = true;
+
+
+if(isAdult) {
+    console.log("Is adult :)")
+} else {
+    console.log("Is not an adult :(");
+}
+//Flip values
+console.log(true);
+console.log(!true); // This means falseS
+
+if(!isAdult) {
+    console.log("Is not an adult :(")
+} else {
+    console.log("Is adult :)");
+}
