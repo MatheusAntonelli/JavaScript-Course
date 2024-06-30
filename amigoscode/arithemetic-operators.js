de9e51bea78b697b9f387ba4b7bd03eedd54d1f3
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-06-30 19:16:59 -0300

    functions

---
+++ amigoscode/arithemetic-operators.js
@@ -0,0 +1,15 @@
+// math
+
+var addition = 2 + 2;
+var subtraction = 2 - 2;
+var division = 10 / 5;
+var multiply = 10 * 2;
+var remainder = 10 % 4;
+var exponentation = 3 ** 234;
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiply);
+console.log(remainder);
+console.log(exponentation);
---
+++ amigoscode/functions.js
@@ -0,0 +1,7 @@
+// Functions
+
+function addNumbers (number1, number2) {
+    var addition = number1 + number2;
+    console.log(addition);
+}
+addNumbers(1,5);
