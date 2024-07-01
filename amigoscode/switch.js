b5efabf42fcdc6e8a53ddb79476d25861e08fdcd
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 16:49:10 -0300

    Create switch.js

---
+++ amigoscode/switch.js
@@ -0,0 +1,15 @@
+// Switch
+
+var gender = "M";
+
+switch (gender) {
+    case "M":
+        console.log("Male");
+        break;
+    case "F":
+        console.log("Female");
+        break;
+    default:
+        console.log("unkown");
+        break;
+}
