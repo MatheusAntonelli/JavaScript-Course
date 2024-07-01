a548df800264639dd175a9888393aba9f7209606
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 15:44:34 -0300

    Create logic-operators.js

---
+++ amigoscode/logic-operators.js
@@ -0,0 +1,7 @@
+// Logical Operators
+// && ||
+// && (AND) || (OR)
+console.log(10 == 10 && "a" == 'a');
+console.log(10 == 11 && "a" == 'a');
+console.log(10 == 110 || "a" == 'a');
+console.log(10 == 10 || "b" == 'a');
