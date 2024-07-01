5c6047d7a1578f39e9568337f1a28e4f737cd550
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 15:57:15 -0300

    Create if-statements.js

---
+++ amigoscode/if-statements.js
@@ -0,0 +1,20 @@
+// If statements
+
+var condition = 1 > 0;
+
+if(condition) {
+    console.log("runs");
+}else {
+    console.log("else condition ran");
+}
+
+
+var gender = "F"
+
+if (gender == "M") {
+    console.log("Male");
+} else if(gender == "F"){
+    console.log("Female");
+}else {
+    console.log("Unkown");
+}
