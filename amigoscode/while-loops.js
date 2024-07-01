57b3a98c890ae0210a98f7d0da2ffaa9798370f7
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 14:29:02 -0300

    Create while-loops.js

---
+++ amigoscode/while-loops.js
@@ -0,0 +1,18 @@
+var names = [
+    "Matheus",
+     "Jamila",
+      "Joe",
+       "Aisha"
+];
+
+var index =0;
+while(index < names.length) {
+    console.log(names[index]);
+    index++;
+}
+
+var condition = true;
+while(condition) {
+    console.log("Hello");
+    condition= false
+}
