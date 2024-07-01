0eb082322f4067502630609d64ebc7f97d90761f
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 15:41:00 -0300

    Create comparison-operators.js

---
+++ amigoscode/comparison-operators.js
@@ -0,0 +1,12 @@
+// Comparison Operators
+// < <= >>= == !=
+//Boolean is true or false
+
+console.log(10 == 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 >= 10);
+console.log(10 != 10);
+
+console.log("a" == "a")
+console.log("a" != "b")
