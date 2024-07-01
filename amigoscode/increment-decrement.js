5fd2b1bcb2dca1bca2875f1bc9d934dfcdb4b353
Matheus Antonelli de Oliveira <matheusantonelli12@gmail.com>
2024-07-01 15:28:29 -0300

    Create increment-decrement.js

---
+++ amigoscode/increment-decrement.js
@@ -0,0 +1,20 @@
+// Increment and Decrement Operators
+
+var number = 0;
+// Postfix increment
+console.log("postfix");
+console.log(number++);
+console.log(number);
+console.log(number--);
+console.log(number);
+
+//prefix
+console.log("Prefix ");
+var numberTwo = 0;
+console.log(++numberTwo);
+console.log(--numberTwo);
+
+console.log("for loop");
+for (var i= 0; i<=10; i++) {
+    console.log(i);
+}
